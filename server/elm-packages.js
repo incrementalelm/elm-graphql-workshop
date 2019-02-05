437c3489a5a67126e9bebec10f36ad02c18c1a94
Dillon Kearns <dillon@dillonkearns.com>
2019-02-05 13:08:00 -0800

    Add elm package server.

---
+++ server/elm-packages.js
@@ -0,0 +1,56 @@
+const { ApolloServer, gql } = require("apollo-server");
+const axios = require("axios");
+let packages = require("./elm-package-cache.json");
+
+axios
+  .get("https://package.elm-lang.org/search.json")
+  .then(function(response) {
+    packages = response.data;
+  })
+  .catch(function(error) {
+    console.log(
+      "Failed to make HTTP request to elm-lang package server.",
+      error
+    );
+  });
+
+const typeDefs = gql`
+  type Package {
+    name: String!
+    summary: String!
+    versions: [String!]!
+  }
+
+  type Query {
+    all: [Package!]!
+    byAuthor(author: String!): [Package!]!
+    find(author: String!, name: String!): Package
+  }
+`;
+
+const resolvers = {
+  Query: {
+    all: () => packages,
+    byAuthor: (parent, args, context) => {
+      return packages.filter(package => {
+        if (package) {
+          return package.name.startsWith(args.author);
+        } else {
+          return false;
+        }
+      });
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  engine: process.env.ENGINE_API_KEY && {
+    apiKey: process.env.ENGINE_API_KEY
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
