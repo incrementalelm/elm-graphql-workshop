99f550fcade9f6e47b8c654459c3dec7fc57c745
Dillon Kearns <dillon@dillonkearns.com>
2019-02-05 14:39:11 -0800

    Update elm packages server.

--- server/elm-packages.js
+++ server/elm-packages.js
@@ -2,6 +2,41 @@ const { ApolloServer, gql } = require("apollo-server");
 const axios = require("axios");
 let packages = require("./elm-package-cache.json");
 
+const quotes = [
+  "Be kind.",
+  "Learn from everyone.",
+  "Collaboration requires communication.",
+  "Not every problem should be solved with code.",
+  "Communication _is_ contribution.",
+  "Understand the problem.",
+  "Explore all possible solutions.",
+  "Pick one.",
+  "Simplicity is not just for beginners.",
+  "It's better to do it _right_ than to do it _right now_.",
+  "It's not done until the docs are great.",
+  "Take responsibility for user experiences.",
+  "Make impossible states impossible.",
+  "There are worse things than being explicit."
+];
+
+const talks = [
+  {
+    title: "Making Impossible States Impossible",
+    minutes: 25,
+    url: "https://www.youtube.com/watch?v=IcgmSRJHu_8"
+  },
+  {
+    title: "The Life of a File",
+    minutes: 47,
+    url: "https://www.youtube.com/watch?v=XpDsk374LDE"
+  },
+  {
+    title: "Scaling Elm Apps",
+    minutes: 59,
+    url: "https://www.youtube.com/watch?v=DoA4Txr4GUs"
+  }
+];
+
 axios
   .get("https://package.elm-lang.org/search.json")
   .then(function(response) {
@@ -15,23 +50,31 @@ axios
   });
 
 const typeDefs = gql`
+  type Query {
+    allPackages: [Package!]!
+    packagesByAuthor(author: String!): [Package!]!
+    findPackage(author: String!, name: String!): Package
+    randomQuote: String!
+    talks: [Talk!]!
+  }
+
   type Package {
     name: String!
     summary: String!
     versions: [String!]!
   }
 
-  type Query {
-    all: [Package!]!
-    byAuthor(author: String!): [Package!]!
-    find(author: String!, name: String!): Package
+  type Talk {
+    title: String!
+    minutes: Int!
+    url: String!
   }
 `;
 
 const resolvers = {
   Query: {
-    all: () => packages,
-    byAuthor: (parent, args, context) => {
+    allPackages: () => packages,
+    packagesByAuthor: (parent, args, context) => {
       return packages.filter(package => {
         if (package) {
           return package.name.startsWith(`${args.author}/`);
@@ -39,7 +82,11 @@ const resolvers = {
           return false;
         }
       });
-    }
+    },
+    randomQuote: () => {
+      return quotes[Math.floor(Math.random() * quotes.length)];
+    },
+    talks: () => talks
   }
 };
 
