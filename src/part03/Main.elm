3b6d045048165d1d91a512f1c806a291e09be256
Dillon Kearns <dillon@dillonkearns.com>
2019-02-05 16:11:04 -0800

    Update instructions.

--- src/part03/Main.elm
+++ src/part03/Main.elm
@@ -15,7 +15,7 @@ import Time
 
 
 type alias Response =
-    List String
+    List ()
 
 
 query : SelectionSet Response RootQuery
@@ -23,9 +23,9 @@ query =
     Query.allPackages packageSelection
 
 
-packageSelection : SelectionSet String ElmStuff.Object.Package
+packageSelection : SelectionSet () ElmStuff.Object.Package
 packageSelection =
-    ElmStuff.Object.Package.name
+    SelectionSet.empty
 
 
 makeRequest : Cmd Msg
@@ -71,7 +71,21 @@ main =
         , queryString = Document.serializeQuery query
         , instructions =
             { title = "Nested Selection Sets"
-            , body = """Now that we're getting the titles of our books, we'd like to get the authors, too! But we also want to practice taking *tiny steps*! Why? Because small steps is what let's us move quickly, steadily, and without mistakes! It might seem unnecessary now, but having this skill at your fingertips is what will make you a master Elm GraphQL query builder!
+            , body = """Wow, that's a lot of {Code|()}'s! Unless you can scan a screen as fast as Commander Data from Star Trek, you probably don't find those too interesting. But it does represent something interesting. The total number of Elm 0.19 packages!
+
+
+Let's use a new tool to turn our returned data into something meaningful. Instead of getting the raw data and then doing something with it, we often prefer to get the data into exactly the meaningful data structure we want with no intermediary format. This is exactly where mapping comes in!
+
+
+| Blockquote
+    SelectionSet.map takes a function and a SelectionSet and calls that function on whatever data comes back in that SelectionSet.
+
+| List
+    (?) Look at the type signature in the docs for {Code|SelectionSet.map}. What does it tell you?
+    -> Using {Code|SelectionSet.map}, change the top-level {Code|SelectionSet} to give us the count of all packages. Note that the type of {Code|Response} will change {Code|Int} instead of a {Code|List ()}.
+
+
+Now that we're getting the titles of our books, we'd like to get the authors, too! But we also want to practice taking *tiny steps*! Why? Because small steps is what let's us move quickly, steadily, and without mistakes! It might seem unnecessary now, but having this skill at your fingertips is what will make you a master Elm GraphQL query builder!
 
 | List
     -> Change the {Code|type alias} for {Code|ElmStuff} to be a record with a *single field in it*. Hint: you'll need to use a function called {Code|SelectionSet.map}.
