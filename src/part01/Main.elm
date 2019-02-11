d6111704e7ab9606d73ca9100cdd88b71f37cfc8
Dillon Kearns <dillon@dillonkearns.com>
2019-02-11 12:03:48 +0100

    Update instructions.

--- src/part01/Main.elm
+++ src/part01/Main.elm
@@ -71,14 +71,23 @@ main =
     -> What happens if you delete {Code|__typename} in the GraphQL query pane?
     (?) How is the data returned different in the {Code|elm-graphql} response and the {Code|gql} response? Why?
 
+| Header
+    Find the Field
 
-
-<>
 Let's turn this {Code|empty} query into something a bit more interesting!
-<>
 
 | List
-    (?) There is exactly one top-level value you can get without doing any nested selection sets. Which is it? Why do you need to nest selection sets for some, but not for this one? Hint: try exploring the {Code|< Docs} pane. Confirm your answer by making the query in the Graphiql editor above and checking that it is valid without a nested Selection Set.
+    (?) There is exactly one top-level value you can get without doing any nested selection sets. Which is it? <> *Hint*: try exploring the {Code|< Docs} pane.
+    -> Confirm your answer by making the query in the Graphiql editor above and checking that it is valid without a nested Selection Set.
+    (?) Why do you need to nest selection sets for some, but not for this one?
+
+
+| Header
+    Can I Write Elm Code Now?
+
+You've waited long enough! Let's build our first query in Elm!
+
+| List
     (?) What do you expect to be the return type in your Elm code when you fetch this? Why?
     -> Now, modify {Code|Main.elm} to fetch the field described above. *Note:* the solution doesn't involve importing any new modules."""
             }
