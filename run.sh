668029be83595ecd5c194004cf4b5a36b9a6a42a
Dillon Kearns <dillon@dillonkearns.com>
2019-02-12 12:56:11 +0100

    Simplify run script.

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash -x
-# npx concurrently "nodemon --watch server ./server/books.js" "elm-live \"$1/Main.elm\" --proxyPrefix /api --proxyHost http://localhost:4000 --open -- --output elm.js"
-# npx concurrently "nodemon --watch server ./server/weather.js" "elm-live \"$1/Main.elm\" --proxyPrefix /api --proxyHost http://localhost:4000 --open -- --output elm.js"
-npx concurrently "nodemon --watch server ./server/elm-stuff.js" "elm-live \"$1/Main.elm\" --proxyPrefix /api --proxyHost http://localhost:4000 --open -- --output elm.js"
+npx concurrently "elm-live \"$1/Main.elm\" --proxyPrefix /api --proxyHost http://localhost:4000 -- --output elm.js" "nodemon -q server/elm-stuff.js"
