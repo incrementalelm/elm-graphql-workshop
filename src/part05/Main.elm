631a2f9fe89da85c57293b0971fa5838a3e5434b
Dillon Kearns <dillon@dillonkearns.com>
2019-02-12 15:28:13 +0100

    Swap two exercises.

--- src/part05/Main.elm
+++ src/part05/Main.elm
@@ -1,10 +1,8 @@
 module Main exposing (main)
 
+import Books.Object.Book
+import Books.Query as Query
 import Browser
-import ElmStuff.Object
-import ElmStuff.Object.Author
-import ElmStuff.Object.Package
-import ElmStuff.Query as Query
 import Graphql.Document as Document
 import Graphql.Http
 import Graphql.Operation exposing (RootQuery)
@@ -12,6 +10,7 @@ import Graphql.OptionalArgument exposing (OptionalArgument(..))
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet, hardcoded, with)
 import Helpers.Main
 import RemoteData exposing (RemoteData)
+import Time
 
 
 type alias Response =
@@ -65,14 +64,50 @@ main =
         , update = update
         , queryString = Document.serializeQuery query
         , instructions =
-            { title = "Combining Selection Sets"
-            , body = """Let's combine the skills we've learned in order to /combine/ some {Code|SelectionSet}s together! What do I mean by combining {Code|SelectionSet}s? Well, it's actually nearly exactly the same as what we learned with {Code|SelectionSet.map}.
+            { title = "Deconstructing Constructors"
+            , body = """This simple Elm concept can really trip you up if you don't take the time to fully understand it. Let's take a moment to demystify Elm constructors!
 
-The only difference is that instead of applying a function to the data that comes back from a single {Code|SelectionSet}, this time you will be applying a function to the data from /two/ different {Code|SelectionSet}s!
+What is a constructor? Well, it's a way to build up data.
+
+| Blockquote
+    1. An Elm constructor is just a function to build data.
+
+    2. The only difference from a regular Elm function is __________________.
+
+
+We'll fill in that blank in a minute. First, let's define a function manually that satisfies the first part.
+
+| Ellie
+    4BVqf9HJc7xa1
+
+
+Okay, so our {Code|buildPerson} function is functionally the same thing as an Elm Constructor. There's just one small difference. Did you guess it? Let's fill in the blank:
+
+
+| Blockquote
+    1. An Elm constructor is just a function to build data.
+
+    2. The only difference from a regular Elm function is *it starts with a capital letter*.
+
+That's right, Elm doesn't let you name your functions starting with a capital letter. So Elm Constructors are the only functions in Elm which start with a capital letter. And you define them in a special way: by defining a type.
+
+
+To wrap up, there are 2 ways that you can define a Type Constructor in Elm:
 
 | List
-    (?) Look at the package docs to find the type signature for {Code|SelectionSet.map2}. This function will be your main tool for this exercise. How does it compare with {Code|SelectionSet.map}?
-    (?) Before you begin: how can you make this task easier on yourself using the techniques for keeping things compiling at each tiny step?
-    -> Define the top-level {Code|query} in {Code|Main.elm} as a {Code|SelectionSet String RootQuery}, where the String is the percentage of Elm packages which were written by the authors of my favorite Elm packages. You can refer back to previous exercises if it's helpful. *Hint*: you may find the built-in Elm function {Code|List.sum} helpful in one spot here!"""
+    # *Record Type Aliases* Example: {Code|type alias User = \\{ name : String, id : Int \\}}. Now the type of the {Code|User} function Elm defines for us is {Code|(String -> Int -> User)}.
+
+    # *Custom Type Constructors* Example: {Code|type User = Guest \\| LoggedInUser String Int}. Now the type of the {Code|LoggedInUser} function Elm defines for us is {Code|String -> Int -> User}. Note that we have two things here. A function called {Code|LoggedInUser}, and a /type/ called {Code|User}. You can't use {Code|User} as a function, nor can you use {Code|LoggedInUser} as a type.
+
+| List
+    (?) What Elm things exist after you define your {Code|type alias} that didn't before?
+    (?) In what context can these things be used?
+
+| Ellie
+    4BTH7sy8zRWa1
+
+| List
+    -> Try using `type alias Person = String`. Does it compile? What does Elm define in this case?
+"""
             }
         }
