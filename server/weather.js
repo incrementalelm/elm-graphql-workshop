6a577f726ba8fa4b992e46036c0de3027ef9d250
Dillon Kearns <dillon@dillonkearns.com>
2019-02-12 16:30:32 +0100

    Update weather API.

--- server/weather.js
+++ server/weather.js
@@ -1,100 +1,42 @@
 const { ApolloServer, gql } = require("apollo-server");
+const axios = require("axios");
+const _ = require("lodash");
+
+const key = "4a6d4fbbebab4dbdb91151515191202";
 
 const typeDefs = gql`
   type Query {
-    currentWeatherByCityName(
-      name: String!
-      countryCode: String
-    ): CurrentWeather!
+    currentWeather: CurrentWeather!
   }
 
   type CurrentWeather {
-    coord: Coordinate
-    weather: [Weather]
-    wind: Wind
-    clouds: Clouds
-    rain: Rain
-    snow: Snow
-    temp: Float!
-    tempMin: Float!
-    tempMax: Float!
-    pressure: Float!
-    humidity: Float!
-    """
-    Sunrise time in specified location, in POSIX UTC date time format.
-    """
-    sunrise: Int!
-
-    """
-    Sunset time in specified location, in POSIX UTC date time format.
-    """
-    sunset: Int!
-
-    """
-    the current time in POSIX date time format.
-    """
-    dt: String
-    id: Int
-    name: String
-    cod: String
-  }
-  type Rain {
-    volumePast3Hours: Float
-  }
-
-  type Snow {
-    volumePast3Hours: Float
-  }
-
-  type Clouds {
-    all: Float
-  }
-
-  type Wind {
-    speed: Float
-    deg: Float
-  }
-
-  # geographic coordinate
-  type Coordinate {
-    # latitude
-    lat: Float
-    # longitude
-    lon: Float
-  }
-
-  type Weather {
-    id: Int
-    main: String
-    description: String
-    icon: String
-    iconURL: String
-  }
-
-  type City {
-    id: Int
-    name: String
-    coord: Coordinate
-    country: String
-  }
+    temperature: Float!
 `;
-
+const CURRENT_WEATHER_URL = "http://api.apixu.com/v1/current.json";
 const resolvers = {
   Query: {
-    currentWeatherByCityName: () => {
-      return {
-        temp: 123
-      };
+    currentWeather: () => {
+      return get(CURRENT_WEATHER_URL, { q: "Oslo" });
     }
+  },
+  CurrentWeather: {
+    temperature: response => response.current.temp_c
   }
 };
 
+function get(url, queries = {}) {
+  const queryString = _.chain({ ...queries, key })
+    .toPairs()
+    .map(p => p.join("="))
+    .join("&")
+    .value();
+  const getUrl = `${url}?${queryString}`;
+  return axios.get(getUrl).then(({ data }) => data);
+}
+
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-  engine: process.env.ENGINE_API_KEY && {
-    apiKey: process.env.ENGINE_API_KEY
-  }
+  resolvers
 });
 
 server.listen().then(({ url }) => {
---
+++ src/part06/Main.elm
@@ -0,0 +1,71 @@
+module Main exposing (main)
+
+import Browser
+import Graphql.Document as Document
+import Graphql.Http
+import Graphql.Operation exposing (RootQuery)
+import Graphql.OptionalArgument exposing (OptionalArgument(..))
+import Graphql.SelectionSet as SelectionSet exposing (SelectionSet, hardcoded, with)
+import Helpers.Main
+import RemoteData exposing (RemoteData)
+import Time
+import Weather.Object.CurrentWeather
+import Weather.Query as Query
+
+
+type alias Response =
+    Float
+
+
+query : SelectionSet Response RootQuery
+query =
+    Query.currentWeatherByCityName identity { name = "Oslo" } Weather.Object.CurrentWeather.temp
+
+
+makeRequest : Cmd Msg
+makeRequest =
+    query
+        |> Graphql.Http.queryRequest "/api"
+        |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
+
+
+
+-- Elm Architecture Setup
+
+
+type Msg
+    = GotResponse Model
+
+
+type alias Model =
+    RemoteData (Graphql.Http.Error Response) Response
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( RemoteData.Loading, makeRequest )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotResponse response ->
+            ( response, Cmd.none )
+
+
+main : Helpers.Main.Program Flags Model Msg
+main =
+    Helpers.Main.document
+        { init = init
+        , update = update
+        , queryString = Document.serializeQuery query
+        , instructions =
+            { title = "Pipelines"
+            , body = """
+"""
+            }
+        }
