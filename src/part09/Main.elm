a9b56d2722615693923238dadbd4b66d3739308c
Dillon Kearns <dillon@dillonkearns.com>
2019-02-15 10:49:22 +0100

    Update instructions.

--- src/part09/Main.elm
+++ src/part09/Main.elm
@@ -35,10 +35,10 @@ compareToHottestDay : Float -> WeatherComparison
 compareToHottestDay currentTemperature =
     let
         difference =
-            hottestDayInCelsius - currentTemperature
+            worldRecordHighInCelsius - currentTemperature
     in
     { today = currentTemperature
-    , hottestDay = hottestDayInCelsius
+    , hottestDay = worldRecordHighInCelsius
     , comparison = String.fromFloat difference ++ " C cooler than the hottest ever recorded temperature."
     }
 
@@ -50,8 +50,8 @@ makeRequest =
         |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
 
 
-hottestDayInCelsius : Float
-hottestDayInCelsius =
+worldRecordHighInCelsius : Float
+worldRecordHighInCelsius =
     56.7
 
 
@@ -93,19 +93,44 @@ main =
             { title = "Custom Scalars"
             , body = """Hmm, something seems off with this code...
 
-Let's bring in some semantic types using our custom scalars!
+Let's bring in some Semantic Types using our custom scalars! When I say Semantic Type, I just mean wrapping a primitive like a {Code|Float} using an Opaque Type. What's an Opaque Type?
 
 | Blockquote
     Opaque Types: a Custom Type in Elm which doesn't expose its Constructor so it can't be Constructed or Deconstructed (Pattern Matched) except through functions defined by that module itself.
 
+For example, you could have {Code|type Currency = Currency Int} where the {Code|Int} represents cents. At the top of the module, you would have {Code|module Currency exposing (Currency, fromCents, toCents)} (not {Code|Currency(..)}, which would publicly expose the constructor). If your internal representation changes, the public interface can stay the same.
+
+| Header
+    Using Our Own Semantic Type
+
+Let's try that ourselves! But instead of creating a {Code|Currency} module, we'll be creating a {Code|Temperature} module. Don't worry about the bug quite yet, we'll get to that in the next section.
+
 | List
-    -> Create a module called {Code|Temperature} under the {Code|src} folder. In it, expose an Opaque Type called {Code|Temperature}. It should have a function called...
-    -> Wrap {Code|hottestDayInCelsius} in a Custom Type Wrapper, {Code|Celsius}. Use the wrapper for
-    -> Change the schema for this server to change it from a {Code|Float} to a Custom Scalar representing its units.
+    -> Create a module called {Code|Temperature} under the {Code|src} folder. In it, expose an Opaque Type that looks like this {Code|type Temperature = Fahrenheit Float \\| Celsius Float}.
+    -> You'll need to write and expose some functions for converting to\\/from {Code|Temperature}. The top of your file should look like this {Code|module Temperature exposing (Temperature, fromCelsius, fromFahrenheit, toCelsius)}.
+    -> Wrap {Code|worldRecordHighInCelsius} using the {Code|Temperature} type, {Code|Celsius} using the functions you exposed for building a {Code|Temperature}. You'll need to change the type in some annotations, and it's a good idea to remove the now-redundant {Code|InCelsius} from the name. Get your code compiling (you'll need to convert one spot to go from a {Code|Temperature} to a {Code|Float}).
+
+
+| Header
+    Fixing the Bug
+
+
+| List
+    -> Open up {Code|server\\/weather\\-custom\\-scalars.js} and take a look at the schema. If you haven't figured it out already, you can see the cause of the bug in the schema definition. Do not change the units on the server-side. Instead, we'll just be using a Custom Scalar to make the units explicit.
+    -> Add a new line to the schema string like this: {Code|scalar Fahrenheit}. The server will automatically restart.
+    -> Regenerate the code by going to the top-level workshop directory and running {Code|npx elm\\-graphql http:\\/\\/localhost:4000\\/api \\-\\-output gen \\-\\-base WeatherCustomScalars}.
+    -> When you recompile your code, you'll get an error. Instead of a Float, you now have a {Code|Fahrenheit} type wrapper. Create a function with the following signature, and use it to do a {Code|SelectionSet.map} to get your code compiling. {Code|scalarToTemperature : Scalar.Fahrenheit -> Float}.
+    -> That's a good first step, and we may have fixed our bug. It's a good practice to defer unwrapping Semantic Types until the last possible moment. Change the type signature of your function to {Code|scalarToTemperature : Scalar.Fahrenheit -> Temperature} and fix the compiler errors.
+
+| Header
+    Bonus
+
+If you still have time, here's a bonus exercise!
 
 | List
     -> Grab the {Code|updatedAt} time in addition to the current data being fetched.
-    -> Install {Code|rtfeldman\\/elm-iso8601-date-strings} by running an {Code|elm install} command from the top-level workshop repo directory.   -> Also install {Code|sporto\\/time-distance}
+    -> Install {Code|rtfeldman\\/elm-iso8601-date-strings} by running an {Code|elm install} command from the top-level workshop repo directory.
+    -> Also install {Code|sporto\\/time-distance}
     -> Use {Code|rtfeldman\\/elm-iso8601-date-strings} to parse the {Code|updatedAt} time, and {Code|sporto\\/time-distance} to map it into a human readable string like "30 seconds ago".
 """
             }
